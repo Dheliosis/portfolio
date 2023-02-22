2069475f4d4efdda5f80c343a9fe5a00599bffeb
Caroline <caroline@codekraft.fr>
2023-02-22 17:18:34 +0100

    Add black coplor and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
 				"primary-blue": "#5CA4EC",
 				"secondary-blue": "#A5C7EA",
 				"dark-blue": "#000F24",
-				"dark-blue-light": "#63799A"
+				"dark-blue-light": "#63799A",
+				"black": "#000"
 			},
 			fontFamily: {
 				Montserrat: ["Montserrat", "ui-sans-serif", "system-ui"],
